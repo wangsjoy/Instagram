6abc548b551be14303e9bf55bfe66ec87651a584
Sophia Joy Wang <wangsj@fb.com>
2021-07-06 13:04:59 -0700

    Added Launch Screen and App Icon

--- Instagram/AppDelegate.m
+++ Instagram/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -16,8 +17,31 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+        configuration.applicationId = @"LDxvc7T8cdCFEB8CYPlhV8w10ieBOKKEvJ71kAHU"; // <- UPDATE
+        configuration.clientKey = @"DZKoBmek1b7Si1072RNfb09KFWViotPpSp0aF2lz"; // <- UPDATE
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+    
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+    gameScore[@"score"] = @1337;
+    gameScore[@"playerName"] = @"Sean Plott";
+    gameScore[@"cheatMode"] = @NO;
+    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+     if (succeeded) {
+            NSLog(@"Object saved!");
+     } else {
+            NSLog(@"Error: %@", error.description);
+     }
+    }];
+    
     return YES;
 }
+
 
 
 #pragma mark - UISceneSession lifecycle
