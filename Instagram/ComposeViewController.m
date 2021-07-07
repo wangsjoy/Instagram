6fa0b10994068552df2d05524b933da9b411f543
Sophia Joy Wang <wangsj@fb.com>
2021-07-07 12:37:42 -0700

    Added Post functionality and photo resizing

--- Instagram/ComposeViewController.m
+++ Instagram/ComposeViewController.m
@@ -7,8 +7,13 @@
 
 #import "ComposeViewController.h"
 #import <UIKit/UIKit.h>
+#import "Post.h"
 
 @interface ComposeViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+@property (nonatomic, strong) Post *post;
+@property (weak, nonatomic) IBOutlet UITextView *captionView;
+@property (nonatomic, strong) UIImage *image;
 
 @end
 
@@ -18,6 +23,19 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    //add gesture recognition to the trailerView element
+    UITapGestureRecognizer *fingerTap =
+      [[UITapGestureRecognizer alloc] initWithTarget:self
+                                              action:@selector(onPhotoTap:)];
+    [self.photoView setUserInteractionEnabled:YES];
+    [self.photoView addGestureRecognizer:fingerTap];
+    
+}
+
+//The event handling method for when photoView element is pressed
+- (void)onPhotoTap:(UITapGestureRecognizer *)recognizer
+{
+    NSLog(@"Photo Tapped");
     UIImagePickerController *imagePickerVC = [UIImagePickerController new];
     imagePickerVC.delegate = self;
     imagePickerVC.allowsEditing = YES;
@@ -34,7 +52,58 @@
     [self presentViewController:imagePickerVC animated:YES completion:nil];
 }
 
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
 
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    [self.photoView setImage:editedImage];
+    
+    //resize photo
+    CGSize size = CGSizeMake(300, 215);
+    UIImage *edited = [self resizeImage:editedImage withSize:(size)];
+    self.image = edited;
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+    NSLog(@"Finished Taking Photo");
+}
+
+- (IBAction)didTapShare:(id)sender {
+    if ((self.photoView != nil) && (![self.captionView.text isEqualToString:@""])){
+        //can post
+        
+        [Post postUserImage:self.image withCaption:self.captionView.text withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
+            if (error){
+                NSLog(@"Error");
+                [self dismissViewControllerAnimated:YES completion:nil];
+            } else {
+                NSLog(@"Successfully posted");
+                [self dismissViewControllerAnimated:YES completion:nil];
+            }
+        }];
+        
+    } else{
+        //don't post
+        NSLog(@"Can't post because empty");
+    }
+}
 
 /*
 #pragma mark - Navigation
---
+++ Instagram/Post.m
@@ -0,0 +1,54 @@
+//
+//  Post.m
+//  Instagram
+//
+//  Created by Sophia Joy Wang on 7/6/21.
+//
+
+#import "Post.h"
+#import <Parse/Parse.h>
+@implementation Post
+    
+@dynamic postID;
+@dynamic userID;
+@dynamic author;
+@dynamic caption;
+@dynamic image;
+@dynamic likeCount;
+@dynamic commentCount;
+
++ (nonnull NSString *)parseClassName {
+    return @"Post";
+}
+
++ (void) postUserImage: ( UIImage * _Nullable )image withCaption: ( NSString * _Nullable )caption withCompletion: (PFBooleanResultBlock  _Nullable)completion {
+    
+    Post *newPost = [Post new];
+    newPost.image = [self getPFFileFromImage:image];
+    newPost.author = [PFUser currentUser];
+    newPost.caption = caption;
+    newPost.likeCount = @(0);
+    newPost.commentCount = @(0);
+    
+    [newPost saveInBackgroundWithBlock: completion];
+}
+
++ (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image {
+ 
+    // check if image is not nil
+    if (!image) {
+        return nil;
+    }
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    // get image data and check if that is not nil
+    if (!imageData) {
+        return nil;
+    }
+    
+//    return [PFFile fileWithName:@"image.png" data:imageData];
+    
+    return [PFFileObject fileObjectWithName:@"image.png" data:imageData];
+}
+
+@end
